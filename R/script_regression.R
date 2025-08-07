b5c1002e295717d25188e2989d943ddb04b2a6b0
alxsrobert <alexis.robert@lshtm.ac.uk>
2025-08-07 21:13:30 +0900

    Add more models
    Add four models to the script:
    - Full model without ethnicity-specific overdispersion
    - ethnicity-only model
    - Full model without household
    - Full model with categorical household
    Save the list of all models

--- R/script_regression.R
+++ R/script_regression.R
@@ -10,7 +10,37 @@ equation_no_household <- as.formula(
 equation_full <- 
   update(equation_no_household, reformulate(c(".", "household_members")))
 
-## Model: all covariates, ethnicity-specific overdispersion
+## Model 1: all covariates, no ethnicity-specific overdispersion
+full <- brm(bf(equation_full), data = part_reg, family = negbinomial)
+
+## Model 2: only ethnicity, ethnicity-specific overdispersion
+ethnicity_with_od <- 
+  brm(bf(tot_contact ~ ethnicity_rural, shape ~ ethnicity_rural),
+      data = part_reg, family = negbinomial)
+
+## Model 3: all covariates, ethnicity-specific overdispersion
 full_withod <- 
   brm(bf(equation_full, shape ~ ethnicity_rural), data = part_reg, 
       family = negbinomial)
+
+## Model 4: all covariates except household size, 
+#           ethnicity-specific overdispersion
+full_withod_nohh <- 
+  brm(bf(equation_no_household, shape ~ ethnicity_rural),
+  data = part_reg, family = negbinomial)
+
+## Model 5: all covariates, categorical household covariate, 
+#           ethnicity-specific overdispersion
+full_with_od_cathh <- 
+  brm(bf(
+    update(equation_no_household, reformulate(c(".", "cat_household_members"))), 
+    shape ~ ethnicity_rural
+  ), data = part_reg, family = negbinomial)
+
+## Save all models
+saveRDS(list(full_no_od = full, 
+             ethnicity = ethnicity_with_od, 
+             full_od = full_withod, 
+             full_od_nohh = full_withod_nohh,
+             full_od_cathh = full_with_od_cathh),
+        paste0("results/regression_output", if(anonymised) "_anoun", ".rds"))
