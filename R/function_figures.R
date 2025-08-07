5398ac2801ab1a951732e3f731cf61b4560c414c
alxsrobert <alexis.robert@lshtm.ac.uk>
2025-08-07 21:53:06 +0900

    Add figure_parameter_model
    TODO: Make the creation of the reference level (and re-ordering of levels in factors) automatic

--- R/function_figures.R
+++ R/function_figures.R
@@ -17,3 +17,76 @@ figure_compare_models <- function(list_regression){
     facet_wrap(.~group, scales = "free", ncol = 2, nrow = 5)
 }
 
+#' Show all parameters from one model (including reference levels)
+#'
+#' @param list_regression 
+#' @param which_model 
+#'
+#' @return ggplot object
+figure_parameter_model <- function(list_regression, which_model){
+  # Create a tibble containing all coefficient estimates and CIs for all models
+  dt_coef <- clean_list_regression_output(list_regression)
+  
+  # Create reference levels for each variable (would be better to automatise this)
+  dt_coef <- rbind.data.frame(
+    dt_coef,
+    cbind.data.frame(
+      model = which_model, term = "18-24", estimate = 1, conf.low = 1,  
+      conf.high = 1, group = "age"),
+    cbind.data.frame(
+      model = which_model, term = "White_Urban", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "ethnicity_rural"),
+    cbind.data.frame(
+      model = which_model, term = "White_Urban", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "shape"),
+    cbind.data.frame(
+      model = which_model, term = "20000-40000", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "income"),
+    cbind.data.frame(
+      model = which_model, term = "hh_size: One", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "household"),
+    cbind.data.frame(
+      model = which_model, term = "Female", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "gender"),
+    cbind.data.frame(
+      model = which_model, term = "weekday", estimate = 1, conf.low = 1, 
+      conf.high = 1, group = "day_of_the_week")
+  )
+  
+  # Re-assign reference levels
+  dt_coef$term <- relevel(dt_coef$term, ref = "hh_size: One")
+  dt_coef$term <- relevel(dt_coef$term, ref = "weekday")
+  dt_coef$term <- relevel(dt_coef$term, ref = "weekend")
+  dt_coef$term <- relevel(dt_coef$term, ref = "Female")
+  dt_coef$term <- relevel(dt_coef$term, ref = "Male")
+  dt_coef$term <- relevel(dt_coef$term, ref = "20000-40000")
+  dt_coef$term <- relevel(dt_coef$term, ref = "Lessthan20000")
+  dt_coef$term <- relevel(dt_coef$term, ref = "18-24")
+  dt_coef$term <- relevel(dt_coef$term, ref = "15-17")
+  dt_coef$term <- relevel(dt_coef$term, ref = "10-14")
+  dt_coef$term <- relevel(dt_coef$term, ref = "5-9")
+  dt_coef$term <- relevel(dt_coef$term, ref = "0-4")
+  
+  # Generate figure after removing Intercept values
+  dt_coef |>
+    filter(model == which_model &
+             !group %in% "intercept" & 
+             !term %in% "(Intercept)" & 
+             !term %in% c("Other", "ethOther", "shape_ethOther")) |> 
+    mutate(group = case_when(
+      group %in% c("gender", "day_of_the_week", "urban_rural") ~ "others",
+      !group %in% c("gender", "day_of_the_week", "urban_rural") ~ group),
+      group = factor(group, levels = c("age", "ethnicity", "ethnicity_rural", 
+                                       "income", "shape", "household", "others"))
+    ) |> 
+    ggplot(aes(x = term, ymin = conf.low, ymax = conf.high, col = model, 
+               group = model)) + 
+    geom_errorbar(width = 0.4, position = position_dodge(0.5)) +
+    geom_point(aes(y = estimate), position = position_dodge(0.5)) + 
+    geom_line(lty = 2, col = "black", y = 1) +
+    facet_wrap(.~group, scales = "free", ncol = 2) + 
+    theme_bw() + ylim(c(.5, 2)) + 
+    xlab("Coefficient") + ylab("Value")
+  
+}
+
--- R/script_regression_figures.R
+++ R/script_regression_figures.R
@@ -4,8 +4,13 @@ source("R/library_and_scripts.R")
 ## Load regression outputs
 list_with_inc <- readRDS(paste0(
   "results/regression_output", if(anonymised) "_anoun", ".rds"))
+which_model <- "full_od_cathh"
+
 ## Compare parameter estimates in the different models
 figure_compare_models(list_with_inc)
+
+## Compare parameter estimates in ethnicity only and full model
+figure_compare_models(list_with_inc[c("ethnicity", "full_od_cathh")])
 
 ## Show the parameter estimates for the full model
 figure_parameter_model(list_with_inc, which_model)
