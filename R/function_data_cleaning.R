26a1d0014209aa5efe2559ca2fb8f492d694cbd1
alxsrobert <alexis3.robert3@gmail.com>
2025-08-28 10:17:51 +0100

    Change how colinearity is fixed
    There's a risk of colinearity between the covariates "p_income", "p_age_group", and "employ": p_income and employ for all (and onlyfor ) individuals below 18 is set to "Child - Not Applicable". Therefore, in the previous version, the model could not distinguish between income-child, employ-child, and age_group-0-4.. This commit fixes this problem.

--- R/function_data_cleaning.R
+++ R/function_data_cleaning.R
@@ -2,37 +2,37 @@ import_and_clean <- function(anonymous = TRUE){
   ## Import contact data
   if(anonymous) {
     part_reg <- readRDS("data/participants_anonymous.RDS")
-    # Risk of co linearity between p_age_group and p_income as the income of all 
-    # individuals aged below 18 is set as "Child - Not Applicable"
-    # To fix this:
-    # - Given age_above_18 the maximum age of the age group containing 18, set 
-    #   the age group of all individuals aged below age_above_18 to "0-age_above_18"
-    # - For all individuals aged under 18, set p_income to their age group instead 
-    #   of "Child - Not Applicable"
-    ## This way, in the regression:
-    # the nb of contact in adults aged between 18 and age_above_18 is intercept * income
-    # the nb of contact in adults above age_above_18 is intercept * age_group * income
-    # the nb of contact in children below 18 is is intercept * age_group
-    min_age_group <- as.numeric(substr(part_reg$p_age_group, 1, 2))
-    age_above_18 <- min(min_age_group[min_age_group > 18], na.rm = TRUE)
-    
-    part_reg <- 
-      part_reg |> 
-      mutate(
-        p_income_ref = p_income,
-        p_age_group_ref = p_age_group,
-        p_income = case_when(
-          grepl("Child", p_income) ~ paste0("child_", p_age_group),
-          .default = p_income
-        ),
-        p_age_group = case_when(
-          grepl("child", p_income) ~ paste0("0-", age_above_18),
-          p_age_group == paste0("18-", age_above_18 - 1) ~ paste0("0-", age_above_18),
-          .default = p_age_group
-        )
-      )
   } else {
   }
+  
+  # Risk of co linearity between p_age_group and p_income as the income and 
+  # employment of all individuals aged below 18 is set as "Child - Not Applicable"
+  # To fix this:
+  # - Use pivot_wider on "employ" and "p_income"
+  # - remove reference levels (respectively "employed" and "20000-39999") AND 
+  #   "Child - Not Applicable"
+  # This way, in the regression:
+  # - p_age group shows the impact of changing only age compared to the reference 
+  #   ("18-24 + employed + 20000-39999")
+  # - no colinearity between age groups below 18 and "Child - Not applicable"
+  part_reg <- part_reg |> 
+    mutate(flag = 1, 
+           id_indiv = seq_len(nrow(part_reg)),
+           p_income = gsub(" |,", "", p_income),
+           p_income = gsub("-", "_", p_income),
+           employ = gsub(" |,", "", employ),
+           employ = gsub("-", "_", employ)
+    ) |> 
+    pivot_wider(names_from = p_income, values_from = flag, 
+                values_fill = 0, names_prefix = "p_income_"
+    ) |> 
+    mutate(flag = 1) |> 
+    pivot_wider(names_from = employ, values_from = flag, values_fill = 0, 
+                names_prefix = "employ_") |> 
+    select(-contains("child")) |> 
+    select(-p_income_20000_39999) |> 
+    select(-employ_employed) |> 
+    mutate(p_age_group = relevel(p_age_group, ref = "18-24"))
   
   return(part_reg)
 }
