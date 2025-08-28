7d8349ecef28c57f3fc1a753272d508d3127fad6
alxsrobert <alexis3.robert3@gmail.com>
2025-08-28 11:23:42 +0100

    Change how reference levels are computed
    Change order of levels + aesthetics

--- R/function_figures.R
+++ R/function_figures.R
@@ -22,15 +22,19 @@ figure_compare_models <- function(list_regression){
 #' @param list_regression list of regression object
 #' @param which_model Model to plot
 #' @param filter_group Group(s) to plot (if set to NULL, all groups are plotted),
-#' set of possible groups: "intercept", "age", "gender", "ethnicity_rural",
-#' "day_of_the_week", "income","employment", "household", "shape".
+#' set of possible groups: "intercept", "age", "ethnicity_rural", "income",
+#' "employment", "household", "shape", "others"
 #'
 #' @return ggplot object
 figure_parameter_model <- function(list_regression, which_model, filter_group = NULL){
   # Create a tibble containing all coefficient estimates and CIs for all models
   dt_coef <- clean_list_regression_output(list_regression)
-  
-  # Create reference levels for each variable (would be better to automatise this)
+  lev_ref <- 
+    unique(c("hh_size: One", "White_Urban", "Employed", "Lessthan20000", 
+             "20000-40000", "Female", "Male", "weekday", "0-4", "5-9",
+             "10-14", "15-17", "18-24", levels(dt_coef$term)))
+
+  # Add reference levels
   dt_coef <- rbind.data.frame(
     dt_coef,
     cbind.data.frame(
@@ -59,42 +63,40 @@ figure_parameter_model <- function(list_regression, which_model, filter_group = 
       conf.high = 1, group = "day_of_the_week")
   )
   
-  # Re-assign reference levels
-  dt_coef$term <- relevel(dt_coef$term, ref = "hh_size: One")
-  dt_coef$term <- relevel(dt_coef$term, ref = "weekday")
-  dt_coef$term <- relevel(dt_coef$term, ref = "weekend")
-  dt_coef$term <- relevel(dt_coef$term, ref = "Female")
-  dt_coef$term <- relevel(dt_coef$term, ref = "Male")
-  dt_coef$term <- relevel(dt_coef$term, ref = "20000-40000")
-  dt_coef$term <- relevel(dt_coef$term, ref = "Lessthan20000")
-  dt_coef$term <- relevel(dt_coef$term, ref = "18-24")
-  dt_coef$term <- relevel(dt_coef$term, ref = "15-17")
-  dt_coef$term <- relevel(dt_coef$term, ref = "10-14")
-  dt_coef$term <- relevel(dt_coef$term, ref = "5-9")
-  dt_coef$term <- relevel(dt_coef$term, ref = "0-4")
+  dt_coef <- dt_coef |>
+    mutate(
+      term = factor(term, levels = lev_ref, ordered = TRUE)
+    )
   
-  if(is.null(filter_group)) filter_group <- unique(dt_coef$group)
   # Generate figure after removing Intercept values
-  dt_coef |>
+  dt_coef_plot <- 
+    dt_coef |>
     filter(model == which_model &
              !group %in% "intercept" & 
              !term %in% "(Intercept)" & 
              !term %in% c("Other", "ethOther", "shape_ethOther")) |> 
-    filter(group %in% filter_group) |>
     mutate(group = case_when(
       group %in% c("gender", "day_of_the_week", "urban_rural") ~ "others",
       !group %in% c("gender", "day_of_the_week", "urban_rural") ~ group),
       group = factor(group, levels = c("age", "ethnicity", "ethnicity_rural", 
                                        "income", "shape", "household", "others"))
-    ) |> 
+    )
+  
+  if(is.null(filter_group)) filter_group <- unique(dt_coef$group)
+  
+  dt_coef_plot |> 
+    filter(group %in% filter_group) |>
     ggplot(aes(x = term, ymin = conf.low, ymax = conf.high, col = model, 
                group = model)) + 
     geom_errorbar(width = 0.4, position = position_dodge(0.5)) +
     geom_point(aes(y = estimate), position = position_dodge(0.5)) + 
     geom_line(lty = 2, col = "black", y = 1) +
     facet_wrap(.~group, scales = "free", 
-               ncol = ifelse(is.null(filter_group), 2, 1)) + 
-    theme_bw() + ylim(c(.3, 2)) + 
+               ncol = ifelse(length(filter_group) > 4, 2, 1)) + 
+    theme_bw() + 
+    ylim(c(min(.3, dt_coef_plot$conf.low), max(2, dt_coef_plot$conf.high))) + 
+    guides(col="none") + 
+    theme(strip.text = element_blank()) +
     xlab("Coefficient") + ylab("Value")
   
 }
