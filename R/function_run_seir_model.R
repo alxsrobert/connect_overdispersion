0718bc70b77ecac01d3cbcda231387c4e7a733b9
alxsrobert <alexis3.robert3@gmail.com>
2025-10-02 16:45:01 +0100

    Function to compute a per capita contact matrix stratified by age, ethnic group, and transmitter group
    The process describing how to compute the per capita matrix can be found in issue #5 (https://github.com/alxsrobert/connect_overdispersion/issues/5)

--- R/function_run_seir_model.R
+++ R/function_run_seir_model.R
@@ -133,3 +133,224 @@ create_contact_group <- function(
   return(list(prop = prop, coef = coef))
 }
 
+#' Generate per capita matrix stratified by age, ethnic group, and transmitter group
+#'
+#' @param n_pop_age_eth_mat 2-d numeric matrix showing the number of inhabitants per 
+#' age group (row) and ethnicity (column).
+#' @param mat_age_per_cap Per capita transmission matrix by age.
+#' @param mat_eth_per_cap Per capita transmission matrix by ethnicity.
+#' @param all_same binary: if TRUE, return an homogeneous mixing per capita 
+#' matrix (i.e. all cells have the same value).
+#' @param n_contact_age_eth_group 2-d numeric matrix: Average number of contacts  
+#' by age-ethnicity (row) and transmitter group (column).
+#' @param prop_indiv_age_eth_group 2-d numeric matrix: Proportion of the population
+#' in each level of age-ethnicity (row) by transmitter group (column) (i.e. 
+#' all(rowSums(prop_indiv_age_eth_group = 1))).
+#' @param k clustering factor for high-contact individuals.
+#'
+#' @return Per capita matrix (numeric matrix)
+get_per_capita <- function(n_pop_age_eth_mat, mat_age_per_cap, mat_eth_per_cap, 
+                           all_same, n_contact_age_eth_group, 
+                           prop_indiv_age_eth_group, k){
+  ## Total population size
+  tot_pop <- sum(n_pop_age_eth_mat)
+  ## number of age groups
+  n_age <- nrow(mat_age_per_cap)
+  ## number of ethnicities
+  n_eth <- nrow(mat_eth_per_cap)
+  ## number of transmitter groups
+  n_group <- ncol(prop_indiv_age_eth_group)
+  
+  ## number of inhabitants per age group
+  n_pop_age_from <- 
+    matrix(rowSums(n_pop_age_eth_mat), nrow = n_age, ncol = n_age)
+  ## number of inhabitants per ethnicity
+  n_pop_eth_from <- 
+    matrix(colSums(n_pop_age_eth_mat), nrow = n_eth, ncol = n_eth)
+  
+  ## Standardise age transmission matrix
+  mat_age_standardised <- mat_age_per_cap * t(n_pop_age_from)
+
+  ### Compute distribution of contacts stratified by both age and ethnicity
+  ## First, create the transmission matrix stratified by both age and ethnicity
+  mat_age_eth <- mat_prop_age_eth <-  
+    matrix(nrow = n_age * n_eth, ncol = n_age * n_eth)
+  for(i in seq_len(nrow(mat_age_eth))){
+    for(j in seq_len(ncol(mat_age_eth))){
+      ## Number of contacts between (age_i;eth_i) and (age_j;eth_j)
+      eth_i <- rep(seq_len(n_eth), n_age)[i]
+      age_i <- rep(seq_len(n_age), each = n_eth)[i]
+      eth_j <- rep(seq_len(n_eth), n_age)[j]
+      age_j <- rep(seq_len(n_age), each = n_eth)[j]
+      
+      # Distribution of ethnicity in participant's age group
+      n_pop_from <- n_pop_age_eth_mat[age_i,]
+      # Distribution of ethnicity in contact's age group
+      n_pop_to <- n_pop_age_eth_mat[age_j,]
+      
+      ## mat_age_standardised[age_i, age_j] is the average number of contacts 
+      ## towards individuals of age_j per individual of age_i
+      ## mat_age_standardised[age_i, age_j] * sum(n_pop_from) is the total
+      ## number of contacts from individuals of age_i towards individuals of age_j
+      n_contact_tot <- mat_age_standardised[age_i, age_j] * sum(n_pop_from)
+      ## Now we have to distribute these contacts between participant and 
+      ## contact ethnicities 
+      
+      # mat_eth_per_cap is the per capita number of contact between ethnicities
+      # mat_eth_per_cap * n_pop_to is the average number of contacts for a participant
+      # mat_eth_per_cap * n_pop_to * n_pop_from is the total number of contacts
+      # between ethnicities
+      contact_per_eth <-
+        mat_eth_per_cap *
+        t(matrix(n_pop_to, n_eth, n_eth)) *
+        matrix(n_pop_from, n_eth, n_eth)
+      # contact_per_eth / sum(contact_per_eth) is the distribution of contacts
+      # in the population. Therefore, prop_contact["Asian", "Asian"] = .1 would 
+      # mean that 10% of all contacts in this population are between individuals of 
+      # Asian ethnicity
+      prop_contact <- contact_per_eth[eth_i, eth_j] / sum(contact_per_eth)
+      
+      # n_contact_tot * prop_contact[] is the total number of contacts between 
+      # age_i and age_j according to RECONNECT distributed among all ethnicities
+      dist_contact <- n_contact_tot * prop_contact
+      
+      # dist_contact is divided by the number of inhabitants in eth_i to get
+      # the number of contact per individual.
+      mat_age_eth[i, j] <- dist_contact / n_pop_from[eth_i]
+      
+    }
+  }
+  
+  # Standardise the resulting contact matrix
+  mat_age_eth_standard <- 
+    .5 * (mat_age_eth * c(t(n_pop_age_eth_mat)) + 
+            t(mat_age_eth * c(t(n_pop_age_eth_mat)))) / 
+    c(t(n_pop_age_eth_mat))
+  
+  ## Then, compute the distribution of contacts for each strata
+  # ie: X11% of all contacts from "age1eth1" go to "age1eth1",
+  #     X12% of all contacts from "age1eth1" go to "age1eth2" etc...
+  for(i in seq_len(nrow(mat_age_eth_standard))){
+    for(j in seq_len(ncol(mat_age_eth_standard))){
+      mat_prop_age_eth[i, ] <- mat_age_eth_standard[i,] / sum(mat_age_eth_standard[i,])
+    }
+  }
+  
+  ## Set column and row names using age group and ethnicity
+  colnames(mat_prop_age_eth) <- rownames(mat_prop_age_eth) <- 
+    paste0("eth", rep(seq_len(n_eth), n_age), 
+           "age", rep(seq_len(n_age), each = n_eth))
+  
+  
+  # CHECK: all row sums of prop_indiv_age_eth_group should be 1
+  if(!all(abs(rowSums(prop_indiv_age_eth_group) - 1) < .001)) 
+    stop("all row sum of prop_indiv_age_eth_group should be 1")
+  
+  ## Compute the transmission matrix stratified by age group / ethnicity / transmitter group
+  ## by combining: 
+  ## 1- the distribution of contacts between age group & ethnicity (mat_prop_age_eth),
+  ## 2- the total number of contacts at each level of age group, ethnicity and 
+  ##    transmitter group (n_contact_age_eth_group),
+  ## 3- the proportion of individuals of an age group and ethnicity that belong 
+  ##    to each transmitter group (prop_indiv_age_eth_group)
+  mat_contact_age_eth_group <-
+    matrix(nrow = n_age * n_eth * n_group, ncol = n_age * n_eth * n_group)
+
+  for(i in seq_len(nrow(mat_contact_age_eth_group))){
+    # Extract the participant's age and ethnicity
+    ageeth_i <- rep(seq_len(n_eth * n_age), n_group)[i]
+    # Extract the participant's transmitter group
+    group_i <- rep(seq_len(n_group), each = n_eth * n_age)[i]
+    
+    ## Extract the distribution of contacts across age and ethnicity given the 
+    ## participants' age and ethnicity
+    prop_age_eth_i <- mat_prop_age_eth[ageeth_i, ]
+    
+    ## Integrate different clustering in high-contact individuals
+    if(group_i == 3){
+      ethnicity_i <- substr(rownames(mat_prop_age_eth)[ageeth_i], 1, 4)
+      prop_age_eth_i[grep(ethnicity_i, names(prop_age_eth_i))] <- 
+        prop_age_eth_i[grep(ethnicity_i, names(prop_age_eth_i))] * k
+      prop_age_eth_i <- prop_age_eth_i/sum(prop_age_eth_i)
+    }
+
+    for(j in seq_len(ncol(mat_contact_age_eth_group))){
+      # Extract the contact's age and ethnicity
+      ageeth_j <- rep(seq_len(n_eth * n_age), n_group)[j]
+      # Extract the contact's transmitter group
+      group_j <- rep(seq_len(n_group), each = n_eth * n_age)[j]
+    
+      # The average number of contacts from {ageeth_i, group_i} towards {ageeth_j}
+      # is computed by multiplying the number of contacts from {ageeth_i, group_i}
+      # by the distribution of contacts from {ageeth_i, group_i}.
+      tot_contact <- 
+        n_contact_age_eth_group[ageeth_i, group_i] * 
+        prop_age_eth_i[ageeth_j]
+      
+      ## We now need to distribute these contacts in the contact's transmitter 
+      ## group:
+      # Distribution of contacts in {age_eth_j} among transmitter group = 
+      #   distribution of number of contacts from ageeth_j among transmitter groups * 
+      #   proportion of ageeth_j that belongs to group_j
+      dist_contact <- 
+        n_contact_age_eth_group[ageeth_j, ] / sum(n_contact_age_eth_group[ageeth_j, ]) *
+        prop_indiv_age_eth_group[ageeth_j, ]
+      ## Normalise dist_contact
+      dist_contact <- dist_contact[group_j] / sum(dist_contact)
+      ## Multiply the number of contact from {ageeth_i, group_i} to {ageeth_j} by
+      ## the proportion of contacts in {ageeth_j} that are directed at {group_j}
+      mat_contact_age_eth_group[i,j] <- tot_contact * dist_contact
+    }
+  }
+  
+  ## Set column and row names of mat_contact_age_eth_group using age group, 
+  ## ethnicity, and transmitter group
+  colnames(mat_contact_age_eth_group) <- rownames(mat_contact_age_eth_group) <- 
+    paste0(rep(rownames(mat_prop_age_eth), n_group), 
+           "group", rep(seq_len(n_group), each = n_eth * n_age))
+  
+  ## CHECK: The number of contacts for each strata of mat_contact_age_eth_group 
+  ## should be equal to n_contact_age_eth_group for this strata (the previous 
+  ## section just "distributed" these contacts among all age groups, ethnicities 
+  ## and transmitter groups)
+  if(any(abs(rowSums(mat_contact_age_eth_group) - c(n_contact_age_eth_group)) > .01)) stop()
+  
+  ## Compute the number of individuals in each strata of age group / ethnicity 
+  ## / transmitter group
+  n_indiv_age_eth_group <- 
+    round(c(prop_indiv_age_eth_group * c(t(n_pop_age_eth_mat))))
+  
+  ## if all_same is true, then the model is homogeneous: all strata have the 
+  ## same number of contact and contact pattern, taken from mean_contact
+  if(all_same) {
+    ## Compute the mean number of contacts per individual in mat_contact_age_eth_group
+    mean_contact <- sum(rowSums(mat_contact_age_eth_group) * n_indiv_age_eth_group / tot_pop)
+    
+    mat_contact_age_eth_group <- 
+      outer(rep(mean_contact, n_eth * n_age * n_group), n_indiv_age_eth_group / tot_pop)
+  }
+  
+  
+  ## Standardise mat_contact_age_eth_group
+  mat_contact_age_eth_group_standard <- 
+    .5 * (mat_contact_age_eth_group * n_indiv_age_eth_group + 
+            t(mat_contact_age_eth_group * n_indiv_age_eth_group)) / 
+    n_indiv_age_eth_group
+  
+  
+  ## Compute the per capita matrix
+  mat_contact_age_eth_group_per_cap <- 
+    t(t(mat_contact_age_eth_group_standard) / n_indiv_age_eth_group)
+  
+  ## Remove empty groups (i.e. transmitter groups with no individual)
+  empty_groups <- which(
+    colSums(is.na(mat_contact_age_eth_group_per_cap)) == n_age * n_eth * n_group
+  )
+  if(length(empty_groups) > 0){
+    mat_contact_age_eth_group_per_cap <- 
+      mat_contact_age_eth_group_per_cap[-empty_groups, -empty_groups]
+  }
+  
+  return(mat_contact_age_eth_group_per_cap) 
+}
+
