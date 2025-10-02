d5038eecb1b77020d16748286185c74ba8e6d2a4
alxsrobert <alexis3.robert3@gmail.com>
2025-10-02 16:45:27 +0100

    Update library and source

--- R/library_and_scripts.R
+++ R/library_and_scripts.R
@@ -2,6 +2,7 @@ library(rio)
 library(tidyverse)
 library(broom.mixed)
 library(brms)
+library(flexmix)
 # Install odin2 and dust2 using:
 # install.packages(
 #   "odin2",
@@ -13,4 +14,5 @@ source("R/function_clean_pop_data.R")
 source("R/function_simulated_pop.R")
 source("R/function_process_regression_outputs.R")
 source("R/function_figures.R")
+source("R/function_run_seir_model.R")
 source("R/function_seir_model.R")
