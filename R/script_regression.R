c0a76a7654764d57cdbce162950226ed5146ede7
alxsrobert <alexis3.robert3@gmail.com>
2026-03-04 17:18:07 +0000

    Minor rephrasing

--- R/script_regression.R
+++ R/script_regression.R
@@ -5,38 +5,35 @@ source("R/library_and_scripts.R")
 part_reg <- import_and_clean(anonymised)
 
 equation_no_household <- update(
-  as.formula(tot_contact ~ p_age_group + p_gender + ethnicity_rural + day_week
-             ),
-  reformulate(c(".", 
-                c(grep("p_income_", colnames(part_reg), value = TRUE)
-                  , grep("employ_", colnames(part_reg), value = TRUE)
-                ))))
+  as.formula(tot_contact ~ p_age_group + p_gender + ethnicity_rural + day_week),
+  reformulate(c(".", c(grep("p_income_", colnames(part_reg), value = TRUE), 
+                       grep("employ_", colnames(part_reg), value = TRUE)))))
 
 # define equations of the regression models
 equation_full <- 
   update(equation_no_household, reformulate(c(".", "household_members")))
 
-## Model 1: all covariates, no ethnicity-specific overdispersion
+## Model 1: all covariates, no ethnicity-specific dispersion
 full <- brm(bf(equation_full), data = part_reg, family = negbinomial)
 
-## Model 2: only ethnicity, ethnicity-specific overdispersion
+## Model 2: only ethnicity, ethnicity-specific dispersion
 ethnicity_with_od <- 
   brm(bf(tot_contact ~ ethnicity_rural, shape ~ ethnicity_rural),
       data = part_reg, family = negbinomial)
 
-## Model 3: all covariates, ethnicity-specific overdispersion
+## Model 3: all covariates, ethnicity-specific dispersion
 full_withod <- 
   brm(bf(equation_full, shape ~ ethnicity_rural), data = part_reg, 
       family = negbinomial)
 
 ## Model 4: all covariates except household size, 
-#           ethnicity-specific overdispersion
+#           ethnicity-specific dispersion
 full_withod_nohh <- 
   brm(bf(equation_no_household, shape ~ ethnicity_rural),
   data = part_reg, family = negbinomial)
 
 ## Model 5: all covariates, categorical household covariate, 
-#           ethnicity-specific overdispersion
+#           ethnicity-specific dispersion
 full_with_od_cathh <- 
   brm(bf(
     update(equation_no_household, reformulate(c(".", "cat_household_members"))), 
