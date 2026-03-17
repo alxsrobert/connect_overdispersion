a91c6d7134c16e097e24edf15a580a95510a01b7
alxsrobert <alexis3.robert3@gmail.com>
2026-03-17 17:13:32 +0000

    Remove unused regression models

--- R/script_regression.R
+++ R/script_regression.R
@@ -13,26 +13,12 @@ equation_no_household <- update(
 equation_full <- 
   update(equation_no_household, reformulate(c(".", "household_members")))
 
-## Model 1: all covariates, no ethnicity-specific dispersion
-full <- brm(bf(equation_full), data = part_reg, family = negbinomial)
-
-## Model 2: only ethnicity, ethnicity-specific dispersion
+## Model 1: only ethnicity, ethnicity-specific dispersion
 ethnicity_with_od <- 
   brm(bf(tot_contact ~ ethnicity_rural, shape ~ ethnicity_rural),
       data = part_reg, family = negbinomial)
 
-## Model 3: all covariates, ethnicity-specific dispersion
-full_withod <- 
-  brm(bf(equation_full, shape ~ ethnicity_rural), data = part_reg, 
-      family = negbinomial)
-
-## Model 4: all covariates except household size, 
-#           ethnicity-specific dispersion
-full_withod_nohh <- 
-  brm(bf(equation_no_household, shape ~ ethnicity_rural),
-  data = part_reg, family = negbinomial)
-
-## Model 5: all covariates, categorical household covariate, 
+## Model 2: all covariates, categorical household covariate, 
 #           ethnicity-specific dispersion
 full_with_od_cathh <- 
   brm(bf(
@@ -42,7 +28,6 @@ full_with_od_cathh <-
 
 ## Save all models
 saveRDS(list(full_no_od = full, 
-             ethnicity = ethnicity_with_od, 
              full_od = full_withod, 
              full_od_nohh = full_withod_nohh,
              full_od_cathh = full_with_od_cathh),
